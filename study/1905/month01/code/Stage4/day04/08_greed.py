698df766ffd752d7c30e929d2508821acc4f0c31
DongHaoDong <dong_1998_dream@163.com>
2019-12-16 18:47:20 +0800

    第八次提交

---
+++ study/1905/month01/code/Stage4/day04/08_greed.py
@@ -0,0 +1,13 @@
+import re
+
+html='''
+<div><p>九霄龙吟惊天变</p></div>
+<div><p>风韵际会潜水游</p></div>
+'''
+# 贪婪匹配
+pattern = re.compile('<div><p>.*</p></div>',re.S)
+r_list = pattern.findall(html)
+# 非贪婪匹配
+pattern = re.compile('<div><p>.*?</p></div>',re.S)
+r_list = pattern.findall(html)
+print(r_list)
